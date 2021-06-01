af98c048fb2d35a7d9376ba6b4e64c5f9d427628
KeishanDejan <keishandejan@gmail.com>
2021-06-01 18:01:45 +0530

    if,elif,else

--- script.py
+++ script.py
@@ -1,8 +1,22 @@
 try:
-    num1 = float(input("Enter First Number: "))
-    op = input("Enter operator: ")
-    num2 = float(input("Enter Second Number:"))
+    def calculate():
+        num1 = float(input("Enter First Number: "))
+        op = input("Enter operator: ")
+        num2 = float(input("Enter Second Number:"))
+        result
+
+    if op == "+":
+        result = num1 + num2
+    elif op == "-":
+        result = num1 - num2
+    elif op == "*":
+        result = num1 * num2
+    elif op == "/":
+        result = num1 / num2
+    else:
+        result = "Invaild Operator"
 
 except:
+
 
 expect:
