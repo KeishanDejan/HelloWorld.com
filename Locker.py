55cebc52e99a66fcd4337665ee0e04a8b61300c1
KeishanDejan <keishandejan@gmail.com>
2021-06-03 17:21:01 +0530

    creating a function and some

--- Locker.py
+++ Locker.py
@@ -1,2 +1,15 @@
 from Classes import user
 
+def locker():
+
+    var User1 = user("Dejan", "password")
+    
+    choice = input("Type 'login' to Login: ")
+    choice = choice.lower()
+    
+    if choice == "login":
+
+    else:
+        
+
+
