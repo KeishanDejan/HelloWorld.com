d62d432166dfdcccde5c880f3a16fc9aac1ad8d5
KeishanDejan <keishandejan@gmail.com>
2021-06-05 19:38:54 +0530

    created a login form

--- Locker.py
+++ Locker.py
@@ -9,6 +9,16 @@ def locker():
     
     if choice == "login":
 
+        # login 
+        var userNameInput = str(input("Enter user name: "))
+        var passwordInput = str(input("Enter password: "))
+
+        if userNameInput == User1.userName and passwordInput == User1.password:
+            
+
+        else:
+            print("UserName or Password is incorrect")
+
     else:
         
 
